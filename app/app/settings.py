26aedce4e62aaf97993f7094d7c228be93324e79
pantgram <pantgr01@gmail.com>
2025-04-15 20:23:57 +0300

    add full text search for articles

--- app/app/settings.py
+++ app/app/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'django.contrib.postgres',
     'django_filters',
     'django_extensions',
     'api',
